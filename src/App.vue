7458ea7a11c4bcc0184de6b790800c0ad5d76a0e
knagara <nagara.keita@gmail.com>
2021-03-17 00:39:43 +0900

    router

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
+    <nav>
+      <router-link to="/" active-class="link--active" exact>Home</router-link>
+      <router-link to="/users" active-class="link--active" exact>Users</router-link>
+    </nav>
     <router-view></router-view>
   </div>
 </template>
+
+<style scoped>
+  .link--active {
+    color: red;
+  }
+</style>
--- src/router.js
+++ src/router.js
@@ -7,6 +7,7 @@ import Users from './views/Users.vue'
 Vue.use(Router);
 
 export default new Router({
+    mode: 'history',
     routes: [{
         path: '/',
         component: Home
